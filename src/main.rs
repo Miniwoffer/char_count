fc70a451d6627e1bcdffbcb550da2c699e1bad14
Odin Hultgren Van Der Horst (Miniwoffer) <odinhorst@hotmail.com>
2019-01-02 19:45:37 +0100

    Inital

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+use std::io::{self,Read};
+
+fn main() {
+    let mut buffer = String::new();
+    let mut counter = HashMap::new();
+    io::stdin().read_to_string(&mut buffer);
+    for c in buffer.chars() {
+        let val = counter.entry(c).or_insert(0);
+        *val += 1;
+    }
+    let mut sorted_vec : Vec<_> = counter.iter().collect();
+    sorted_vec.sort_unstable_by(|(ac,ai),(bc,bi)| bi.cmp(ai));
+    for (c,i) in &sorted_vec {
+        println!("{:?}: {}",c,i);
+    }
+}
